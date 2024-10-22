8b3ed0e73ce287179ed44dca3b9585f67ff24a97
moelshenawy <mohamedgk5@gmail.com>
2024-10-22 09:42:13 +0300

    mobile resposive

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const fs = require("fs");
-const path = require("path");
-const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 let nextConfig = {
@@ -15,30 +13,6 @@ let nextConfig = {
       { protocol: "https", hostname: "zamakanweb1.suwa.io" },
     ],
   },
-
-  i18n,
 };
-
-try {
-  const languagesPath = path.join(__dirname, "public/locales.json");
-  const locales = JSON.parse(fs.readFileSync(languagesPath, "utf8"));
-
-  nextConfig.i18n = {
-    locales: locales.map((lang) => lang),
-    defaultLocale: "ar",
-    localeDetection: false,
-  };
-} catch (error) {
-  console.warn(
-    "Could not dynamically load locales, using default settings:",
-    error,
-    locales
-  );
-  nextConfig.i18n = {
-    locales: ["ar", "en"], // Fallback locales list, including Arabic and English
-    defaultLocale: "ar",
-    localeDetection: false,
-  };
-}
 
 module.exports = nextConfig;
--- src/components/DraggableBox/index.jsx
+++ src/components/DraggableBox/index.jsx
@@ -1,75 +1,315 @@
+// import React, { useState, useEffect, useRef } from 'react';
+// import { motion, useAnimation } from "framer-motion";
+// import styles from './index.module.scss'
+// import { IoClose } from "react-icons/io5";
+// import { RiArrowUpSLine } from "react-icons/ri";
+// import { RiArrowDownSLine } from "react-icons/ri";
+// import { RiArrowUpDoubleFill } from "react-icons/ri";
+
+// function BottomSheet({ isClosed, setIsClosed, currentVideo
+//   , setCurrentVideo }) {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   function onClose() {
+//     setIsOpen(false);
+//   }
+
+//   function onOpen() {
+//     setIsOpen(true);
+//   }
+
+//   function onToggle() {
+//     setIsOpen(!isOpen);
+//   }
+
+//   const prevIsOpen = usePrevious(isOpen);
+//   const controls = useAnimation();
+
+//   function onDragEnd(event, info) {
+//     const shouldClose =
+//       info.velocity.y > 20 || (info.velocity.y >= 0 && info.point.y > 45);
+//     if (shouldClose) {
+//       controls.start("hidden");
+//       onClose();
+//     } else {
+//       controls.start("visible");
+//       onOpen();
+//     }
+//   }
+
+//   useEffect(() => {
+//     if (prevIsOpen && !isOpen) {
+//       controls.start("hidden");
+//     } else if (!prevIsOpen && isOpen) {
+//       controls.start("visible");
+//     } else if (isClosed) {
+//       controls.start("closed");
+//     } else if (!isClosed) {
+//       controls.start("hidden");
+//     }
+//   }, [controls, isOpen, isClosed, prevIsOpen]);
+
+//   const handleDoubleClick = (e) => {
+//     switch (e.detail) {
+//       case 1:
+//         // console.log("click");
+//         break;
+//       case 2:
+//         if (!prevIsOpen && isOpen) {
+//           controls.start("hidden");
+//           setIsOpen(false);
+//           // console.log("double click visible");
+//         } else if (prevIsOpen && !isOpen) {
+//           controls.start("visible");
+//           setIsOpen(true);
+
+//           // console.log("double click hidden");
+//         }
+
+//         break;
+
+//       default:
+//         return;
+//     }
+//   };
+
+//   return (
+//     <motion.div
+//       drag="y"
+//       onDragEnd={onDragEnd}
+//       initial="hidden"
+//       animate={controls}
+//       className={'bottom_sheet_container'}
+
+//       transition={{
+//         type: "spring",
+//         damping: 40,
+//         stiffness: 400
+//       }}
+//       variants={{
+//         // hidden: { y: "calc(100% - 256px)" },
+//         // visible: { y: "10%" },
+//         visible: { y: 104 },
+//         hidden: { y: "70%" },
+//         closed: { y: "100%" }
+
+
+//       }}
+//       dragConstraints={{ top: 0 }}
+//       dragElastic={0.2}
+//       style={{
+//         display: "inline-block",
+//         backgroundColor: "white",
+//         width: 320,
+//         height: 768,
+//         border: "1px solid #E0E0E0",
+//         boxShadow:
+//           "0px 2px 5px rgba(0, 0, 0, 0.06), 0px 2px 13px rgba(0, 0, 0, 0.12)",
+//         borderRadius: "13px 13px 0px 0px",
+//         overflow: "hidden",
+//         zIndex: 1000
+//       }}
+//     >
+//       <div className="DragHandleEdge" onClick={handleDoubleClick}>
+//         <div className="DragHandle" />
+//       </div>
+//       <div className="Navbar">
+//         <span className="Title">أسئلة أخرى</span>
+//         <div className="ButtonGroup">
+//           <span className="ButtonExpandCollapse" onClick={onToggle}>
+//             <span className="IconExpandCollapse"> {isOpen ? <RiArrowDownSLine /> : <RiArrowUpSLine />}</span>
+//             <span className="LabelExpandCollapse">
+//               {isOpen ? "اغلاق" : "فتح"}
+//             </span>
+//           </span>
+//           <span className="CloseIcon" onClick={() => setIsClosed(true)}>
+//             <IoClose />
+//           </span>
+//         </div>
+
+
+
+//       </div>
+
+//       <div className="btns_container">
+
+//         <button className={currentVideo === 'https://toot.one/rawai/2.mp4' ? 'active' : ''}
+//           onClick={() => { setCurrentVideo('https://toot.one/rawai/2.mp4'); setIsClosed(true) }}> الى أي العصور يعود قصر عروة ؟</button>
+//         <button className={currentVideo === 'https://toot.one/rawai/3.mp4' ? 'active' : ''}
+//           onClick={() => { setCurrentVideo('https://toot.one/rawai/3.mp4'); setIsClosed(true) }}> أين تقع قصور عروة بن الزبير ؟</button>
+//         <button className={currentVideo === 'https://toot.one/rawai/4.mp4' ? 'active' : ''}
+//           onClick={() => { setCurrentVideo('https://toot.one/rawai/4.mp4'); setIsClosed(true) }}>ما هي أهم المعالم الأثرية ف قصور عروة بن الزبير ؟</button>
+//       </div>
+
+//     </motion.div >
+//   );
+// }
+
+// function usePrevious(value) {
+//   const previousValueRef = useRef();
+
+//   useEffect(() => {
+//     previousValueRef.current = value;
+//   }, [value]);
+
+//   return previousValueRef.current;
+// }
+
+// const DraggableBox = () => {
+//   const [isClosed, setIsClosed] = useState(true);
+//   const [currentVideo, setCurrentVideo] = useState('https://toot.one/rawai/1.mp4');
+//   const [muted, setMuted] = useState(true); // Start muted initially
+
+
+
+
+//   const videoRef = useRef(null);
+
+
+
+
+//   // Unmute the video on user interaction
+//   const handleUnmute = () => {
+//     if (videoRef.current) {
+//       setMuted(false); // Unmute video
+//       videoRef.current.muted = false;
+//     }
+//   };
+
+//   const handleVideoEnd = () => {
+//     if (currentVideo === '1') {
+//       setIsSheetOpen(true);
+//     }
+//   };
+
+
+
+//   const videoRef2 = useRef(null);
+
+//   useEffect(() => {
+//     if (videoRef2.current) {
+
+//       videoRef2.current.play();
+//     }
+//   }, []);
+
+
+
+//   const handleUnmute2 = () => {
+//     if (videoRef2.current) {
+//       setMuted(false); // Unmute video
+//       videoRef2.current.muted = false;
+//     }
+//   };
+
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         flexDirection: "column",
+//         background: "white",
+//         borderRadius: 10,
+//         width: 320,
+//         height: "90vh",
+//         maxHeight: 728,
+//         overflowY: "hidden",
+//         filter:
+//           "drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.18)) drop-shadow(0px 8px 13px rgba(0, 0, 0, 0.06)) drop-shadow(0px 13px 34px rgba(0, 0, 0, 0.12))"
+//       }}
+//     >
+
+
+
+
+//       {currentVideo === 'https://toot.one/rawai/1.mp4' ?
+//         <>
+//           <div className={styles.video_container} onClick={handleUnmute2}>
+//             {/* <span className="Title">Untitled demo</span> */}
+//             {/* <span className="Caption">All changes saved</span> */}
+//             <video width="320" height="240"
+//               ref={videoRef2}
+//               controls
+//               playsInline
+//               autoPlay
+
+//             >
+//               <source src="https://toot.one/rawai/1.mp4" type="video/mp4" />
+//               Your browser does not support the video tag.
+//             </video>
+//           </div>
+//         </>
+
+
+//         :
+//         ''
+//       }
+
+
+
+//       <div className={styles.video_container} onClick={handleUnmute}>
+//         <video
+//           ref={videoRef}
+//           className={styles.videoElement}
+//           src={`${currentVideo}`}
+//           onEnded={handleVideoEnd}
+//           controls
+//           playsInline
+//           autoPlay
+//         />
+//       </div>
+//       <button className="ButtonOpenSheet" onClick={() => setIsClosed(false)}>
+//         <RiArrowUpDoubleFill />
+//       </button>
+//       <BottomSheet isClosed={isClosed} setIsClosed={setIsClosed} currentVideo={currentVideo}
+//         setCurrentVideo={setCurrentVideo} />
+//     </div >
+//   )
+// }
+
+// export default DraggableBox
+
+
+
 import React, { useState, useEffect, useRef } from 'react';
-import { motion, useAnimation } from "framer-motion";
-import styles from './index.module.scss'
-import { IoClose } from "react-icons/io5";
-import { RiArrowUpSLine } from "react-icons/ri";
-import { RiArrowDownSLine } from "react-icons/ri";
-import { RiArrowUpDoubleFill } from "react-icons/ri";
+import { motion, useAnimation } from 'framer-motion';
+import styles from './index.module.scss';
+import { IoClose } from 'react-icons/io5';
+import { RiArrowUpSLine, RiArrowDownSLine, RiArrowUpDoubleFill } from 'react-icons/ri';
 
-function BottomSheet({ isClosed, setIsClosed, currentVideo
-  , setCurrentVideo }) {
+function BottomSheet({ isClosed, setIsClosed, currentVideo, setCurrentVideo }) {
   const [isOpen, setIsOpen] = useState(false);
-
-  function onClose() {
-    setIsOpen(false);
-  }
-
-  function onOpen() {
-    setIsOpen(true);
-  }
-
-  function onToggle() {
-    setIsOpen(!isOpen);
-  }
 
   const prevIsOpen = usePrevious(isOpen);
   const controls = useAnimation();
 
   function onDragEnd(event, info) {
-    const shouldClose =
-      info.velocity.y > 20 || (info.velocity.y >= 0 && info.point.y > 45);
+    const shouldClose = info.velocity.y > 20 || (info.velocity.y >= 0 && info.point.y > 45);
     if (shouldClose) {
-      controls.start("hidden");
-      onClose();
+      controls.start('hidden');
+      setIsOpen(false);
     } else {
-      controls.start("visible");
-      onOpen();
+      controls.start('visible');
+      setIsOpen(true);
     }
   }
 
   useEffect(() => {
-    if (prevIsOpen && !isOpen) {
-      controls.start("hidden");
-    } else if (!prevIsOpen && isOpen) {
-      controls.start("visible");
-    } else if (isClosed) {
-      controls.start("closed");
-    } else if (!isClosed) {
-      controls.start("hidden");
+    if (isClosed) {
+      controls.start('closed');
+    } else {
+      controls.start('hidden');
     }
-  }, [controls, isOpen, isClosed, prevIsOpen]);
+  }, [controls, isClosed]);
 
   const handleDoubleClick = (e) => {
-    switch (e.detail) {
-      case 1:
-        // console.log("click");
-        break;
-      case 2:
-        if (!prevIsOpen && isOpen) {
-          controls.start("hidden");
-          setIsOpen(false);
-          // console.log("double click visible");
-        } else if (prevIsOpen && !isOpen) {
-          controls.start("visible");
-          setIsOpen(true);
-
-          // console.log("double click hidden");
-        }
-
-        break;
-
-      default:
-        return;
+    if (e.detail === 2) {
+      if (isOpen) {
+        controls.start('hidden');
+        setIsOpen(false);
+      } else {
+        controls.start('visible');
+        setIsOpen(true);
+      }
     }
   };
 
@@ -80,34 +320,28 @@ function BottomSheet({ isClosed, setIsClosed, currentVideo
       initial="hidden"
       animate={controls}
       className={'bottom_sheet_container'}
-
       transition={{
-        type: "spring",
+        type: 'spring',
         damping: 40,
-        stiffness: 400
+        stiffness: 400,
       }}
       variants={{
-        // hidden: { y: "calc(100% - 256px)" },
-        // visible: { y: "10%" },
         visible: { y: 104 },
-        hidden: { y: "70%" },
-        closed: { y: "100%" }
-
-
+        hidden: { y: '70%' },
+        closed: { y: '100%' },
       }}
       dragConstraints={{ top: 0 }}
       dragElastic={0.2}
       style={{
-        display: "inline-block",
-        backgroundColor: "white",
+        display: 'inline-block',
+        backgroundColor: 'white',
         width: 320,
         height: 768,
-        border: "1px solid #E0E0E0",
-        boxShadow:
-          "0px 2px 5px rgba(0, 0, 0, 0.06), 0px 2px 13px rgba(0, 0, 0, 0.12)",
-        borderRadius: "13px 13px 0px 0px",
-        overflow: "hidden",
-        zIndex: 1000
+        border: '1px solid #E0E0E0',
+        boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.06), 0px 2px 13px rgba(0, 0, 0, 0.12)',
+        borderRadius: '13px 13px 0px 0px',
+        overflow: 'hidden',
+        zIndex: 1000,
       }}
     >
       <div className="DragHandleEdge" onClick={handleDoubleClick}>
@@ -116,125 +350,119 @@ function BottomSheet({ isClosed, setIsClosed, currentVideo
       <div className="Navbar">
         <span className="Title">أسئلة أخرى</span>
         <div className="ButtonGroup">
-          <span className="ButtonExpandCollapse" onClick={onToggle}>
-            <span className="IconExpandCollapse"> {isOpen ? <RiArrowDownSLine /> : <RiArrowUpSLine />}</span>
-            <span className="LabelExpandCollapse">
-              {isOpen ? "اغلاق" : "فتح"}
+          <span className="ButtonExpandCollapse" onClick={() => setIsOpen(!isOpen)}>
+            <span className="IconExpandCollapse">
+              {isOpen ? <RiArrowDownSLine /> : <RiArrowUpSLine />}
             </span>
+            <span className="LabelExpandCollapse">{isOpen ? 'اغلاق' : 'فتح'}</span>
           </span>
           <span className="CloseIcon" onClick={() => setIsClosed(true)}>
             <IoClose />
           </span>
         </div>
-
-
-
       </div>
 
       <div className="btns_container">
-
-        <button className={currentVideo === 'https://toot.one/rawai/2.mp4' ? 'active' : ''}
-          onClick={() => { setCurrentVideo('https://toot.one/rawai/2.mp4'); setIsClosed(true) }}> الى أي العصور يعود قصر عروة ؟</button>
-        <button className={currentVideo === 'https://toot.one/rawai/3.mp4' ? 'active' : ''}
-          onClick={() => { setCurrentVideo('https://toot.one/rawai/3.mp4'); setIsClosed(true) }}> أين تقع قصور عروة بن الزبير ؟</button>
-        <button className={currentVideo === 'https://toot.one/rawai/4.mp4' ? 'active' : ''}
-          onClick={() => { setCurrentVideo('https://toot.one/rawai/4.mp4'); setIsClosed(true) }}>ما هي أهم المعالم الأثرية ف قصور عروة بن الزبير ؟</button>
+        <button
+          className={currentVideo === 'https://toot.one/rawai/2.mp4' ? 'active' : ''}
+          onClick={() => {
+            setCurrentVideo('https://toot.one/rawai/2.mp4');
+            setIsClosed(true);
+          }}
+        >
+          الى أي العصور يعود قصر عروة ؟
+        </button>
+        <button
+          className={currentVideo === 'https://toot.one/rawai/3.mp4' ? 'active' : ''}
+          onClick={() => {
+            setCurrentVideo('https://toot.one/rawai/3.mp4');
+            setIsClosed(true);
+          }}
+        >
+          أين تقع قصور عروة بن الزبير ؟
+        </button>
+        <button
+          className={currentVideo === 'https://toot.one/rawai/4.mp4' ? 'active' : ''}
+          onClick={() => {
+            setCurrentVideo('https://toot.one/rawai/4.mp4');
+            setIsClosed(true);
+          }}
+        >
+          ما هي أهم المعالم الأثرية في قصور عروة بن الزبير ؟
+        </button>
       </div>
-
-    </motion.div >
+    </motion.div>
   );
 }
 
 function usePrevious(value) {
   const previousValueRef = useRef();
-
   useEffect(() => {
     previousValueRef.current = value;
   }, [value]);
-
   return previousValueRef.current;
 }
 
 const DraggableBox = () => {
-  const [isClosed, setIsClosed] = useState(false);
+  const [isClosed, setIsClosed] = useState(true);
   const [currentVideo, setCurrentVideo] = useState('https://toot.one/rawai/1.mp4');
-  const [muted, setMuted] = useState(true); // Start muted initially
-
-
-
-
   const videoRef = useRef(null);
-
-  // useEffect(() => {
-  //   if (videoRef.current) {
-  //     videoRef.current.play();
-  //   }
-  // }, [currentVideo]);
-
+  const [muted, setMuted] = useState(true);
 
   useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.play().catch((error) => {
-        console.error('Error attempting to play:', error);
-      });
-    }
-  }, [currentVideo]);
 
-  // Unmute the video on user interaction
+    if (videoRef.current) {
+      videoRef.current.muted = muted; // Set the initial muted state
+
+      videoRef.current.play().catch((error) => {
+        console.error('Autoplay failed:', error);
+      });
+
+    }
+  }, [currentVideo, muted]);
+
+
+
   const handleUnmute = () => {
     if (videoRef.current) {
-      setMuted(false); // Unmute video
-      videoRef.current.muted = false;
-      videoRef.current.play(); // Replay to trigger audio
+      setMuted(false); // Unmute the video
+
+      if (muted === true) {
+        videoRef.current.muted = false; // Unmute video element
+        videoRef.current.play(); // Trigger play again in case autoplay was blocked
+      }
     }
   };
 
-  const handleVideoEnd = () => {
-    if (currentVideo === '1') {
-      setIsSheetOpen(true);
-    }
-  };
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "column",
-        background: "white",
-        borderRadius: 10,
-        width: 320,
-        height: "90vh",
-        maxHeight: 728,
-        overflowY: "hidden",
-        filter:
-          "drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.18)) drop-shadow(0px 8px 13px rgba(0, 0, 0, 0.06)) drop-shadow(0px 13px 34px rgba(0, 0, 0, 0.12))"
       }}
+      className={styles.main_container}
     >
-
       <div className={styles.video_container} onClick={handleUnmute}>
-        {/* <span className="Title">Untitled demo</span> */}
-        {/* <span className="Caption">All changes saved</span> */}
-
         <video
+          key={currentVideo}
           ref={videoRef}
           className={styles.videoElement}
-          src={`${currentVideo}`}
-          onEnded={handleVideoEnd}
+          src={currentVideo}
           controls
-          playsInline
           autoPlay
+          playsInline
+          muted={false}
         />
-
       </div>
       <button className="ButtonOpenSheet" onClick={() => setIsClosed(false)}>
         <RiArrowUpDoubleFill />
       </button>
-      <BottomSheet isClosed={isClosed} setIsClosed={setIsClosed} currentVideo={currentVideo}
-        setCurrentVideo={setCurrentVideo} />
+      <BottomSheet
+        isClosed={isClosed}
+        setIsClosed={setIsClosed}
+        currentVideo={currentVideo}
+        setCurrentVideo={setCurrentVideo}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default DraggableBox
-
-
-
+export default DraggableBox;
